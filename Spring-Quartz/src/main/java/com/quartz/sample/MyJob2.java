5c316dfc9862d8c298df63db699aebb16143cce6
随风飘扬的笑 <18217610763@163.com>
2018-05-13 18:05:41 +0800

    Update MyJob2.java

--- Spring-Quartz/src/main/java/com/quartz/sample/MyJob2.java
+++ Spring-Quartz/src/main/java/com/quartz/sample/MyJob2.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * 工作类：继承QuartzJobBean，或者实现Job接口
+ * 任务类：继承QuartzJobBean，或者实现Job接口
  */
 public class MyJob2 extends QuartzJobBean {
 
