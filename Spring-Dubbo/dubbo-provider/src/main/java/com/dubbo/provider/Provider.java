6a11643f8cdb592e8298bb9d23288faab7e7f3bd
随风飘扬的笑 <18217610763@163.com>
2018-05-17 23:48:46 +0800

    Update Provider.java

--- Spring-Dubbo/dubbo-provider/src/main/java/com/dubbo/provider/Provider.java
+++ Spring-Dubbo/dubbo-provider/src/main/java/com/dubbo/provider/Provider.java
@@ -1,13 +1,13 @@
 package com.dubbo.provider;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.ApplicationContext;
 import java.io.IOException;
 
 public class Provider {
 
    public static void main(String[] args) throws IOException {
-       ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-dubbo-provider.xml");
+       ApplicationContext  ac = new ClassPathXmlApplicationContext("classpath:spring-dubbo-provider.xml");
        ac.start();
        System.out.println("服务启动成功......");
        System.in.read();// 按任意键退出
